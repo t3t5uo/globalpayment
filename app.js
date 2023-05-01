ec8509bf4e84dd3610788d3791f25f512b43ea2c
Alex Dale <alexdale94@gmail.com>
2023-05-01 19:46:46 +0700

    change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const crypto = require("crypto");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   const merchantId = "dev150982075579245497";
